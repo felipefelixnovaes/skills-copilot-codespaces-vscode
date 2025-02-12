dfae1f26146f970a15ccc119a98e5c9fe1c1b772
felipefelixnovaes <5013851@alunouninter.com>
2025-02-12 12:44:52 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  const skills = {
+    html: 'Advanced',
+    css: 'Advanced',
+    js: 'Advanced',
+    react: 'Beginner',
+    node: 'Beginner',
+    git: 'Advanced',
+  };
+
+  return skills;
+}
